689677a8b84b46567069b87ecdad9002d41ac484
gavriel <gpappas001@stpaul.k12.mn.us>
2022-11-09 16:21:31 -0600

    start turn degree command

---
+++ src/main/java/frc/robot/commands/TurnDegree.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+public class TurnDegree extends CommandBase{
+
+    DriveTrain m_driveTrain;
+    
+    
+}
