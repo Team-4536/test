ba079b3358c3074e53a30eda54f43c0ceba9059f
gavriel <gpappas001@stpaul.k12.mn.us>
2022-11-09 17:04:06 -0600

    comment file gavriel nicholas gavriel robot dotob

--- src/main/java/frc/robot/commands/TurnDegree.java
+++ src/main/java/frc/robot/commands/TurnDegree.java
@@ -10,7 +10,7 @@ public class TurnDegree extends CommandBase{
     DriveTrain m_driveTrain;
     Gyroscope m_gyroscope;
     Double m_goalAngle;
-
+    
     public TurnDegree(Gyroscope gav, DriveTrain gav2, Double angle){
 
         m_gyroscope = gav;
@@ -18,14 +18,14 @@ public class TurnDegree extends CommandBase{
         m_goalAngle = angle;
 
     }
-
+    //gets gyroscope and drivetrain and the angle we want into turndegree
     @Override
     public void initialize() {
       
         m_gyroscope.resetGyroscope();
 
     }
-
+    //reset gyroscope
     @Override
     public void execute() {
    
@@ -33,13 +33,15 @@ public class TurnDegree extends CommandBase{
 
     }
 
+
+    //this stops the robot once the goal angle is reached
     @Override
     public boolean isFinished() {
         
         return (m_gyroscope.getAngle() >= m_goalAngle);
 
     }
-
+    //returns true if the angle is at or above goal angle meaning turn is finished
 
     
     
