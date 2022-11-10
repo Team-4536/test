53ce71e2c70f101dfcc0b004da40b9d5ff05e986
Gavriel Pappas <gpappas001@stpaul.k12.mn.us>
2022-11-10 13:41:56 -0600

    remove unused imports COLOR THEMES

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,11 +1,8 @@
 package frc.robot.subsystems;
 
-import javax.swing.plaf.basic.BasicTabbedPaneUI.TabSelectionHandler;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
