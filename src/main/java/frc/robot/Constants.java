b7480de20ef3472d079c965a0408c0fbd0d017ef
Robbie <robbiemaldrich@gmail.com>
2022-11-21 15:48:37 -0600

    modified:   src/main/java/frc/robot/Constants.java 	modified:   src/main/java/frc/robot/commands/DriveForward.java 	modified:   src/main/java/frc/robot/subsystems/DriveTrain.java 	modified:   src/main/java/frc/robot/subsystems/DriveTrain.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,41 +17,41 @@ import edu.wpi.first.wpilibj.CounterBase.EncodingType;
  
 public final class Constants {
 
-    public final static class DriveInfo{
+    public final static class DriveInfo {
 
-    public final static int ENCODER_TICKS_PER_ROTATION = 360;
+        public final static int ENCODER_TICKS_PER_ROTATION = 360;
 
-    public final static double MECANUM_WHEEL_CIRCUMFERENCE_IN_INCHES = 19.0;
-    public final static double MECANUM_WHEEL_DIAMETER_IN_INCHES = MECANUM_WHEEL_CIRCUMFERENCE_IN_INCHES/Math.PI;
+        public final static double MECANUM_WHEEL_CIRCUMFERENCE_IN_INCHES = 19.0;
+        public final static double MECANUM_WHEEL_DIAMETER_IN_INCHES = MECANUM_WHEEL_CIRCUMFERENCE_IN_INCHES/Math.PI;
 
-    // motor port value constants
-    public static final int LEFT_FRONT_DRIVE_MOTOR_ID = 0;
-    public static final int LEFT_REAR_DRIVE_MOTOR_ID = 3;
-    public static final int RIGHT_FRONT_DRIVE_MOTOR_ID = 1;
-    public static final int RIGHT_REAR_DRIVE_MOTOR_ID = 2;
-   // public static final MotorType DRIVE_MOTOR_BRUSHED_TYPE = MotorType.kBrushed;
+        // motor port value constants
+        public static final int LEFT_FRONT_DRIVE_MOTOR_ID = 0;
+        public static final int LEFT_REAR_DRIVE_MOTOR_ID = 3;
+        public static final int RIGHT_FRONT_DRIVE_MOTOR_ID = 1;
+        public static final int RIGHT_REAR_DRIVE_MOTOR_ID = 2;
+        // public static final MotorType DRIVE_MOTOR_BRUSHED_TYPE = MotorType.kBrushed;
 
-    //public static final boolean LEFT_DRIVE_MOTORS_ARE_INVERTED = true;
-    //public static final boolean RIGHT_DRIVE_MOTORS_ARE_INVERTED = false;
+        //public static final boolean LEFT_DRIVE_MOTORS_ARE_INVERTED = true;
+        //public static final boolean RIGHT_DRIVE_MOTORS_ARE_INVERTED = false;
 
-    //public static final double DIFFERENTIAL_DRIVE_DEADBAND = 0.4;
+        //public static final double DIFFERENTIAL_DRIVE_DEADBAND = 0.4;
 
-    //encoder port values (?)
+        //encoder port values (?)
 
-    public static final int LEFT_DRIVE_ENCODER_CHANNEL_A = 0;
-    public static final int LEFT_DRIVE_ENCODER_CHANNEL_B = 1;
-    public static final int RIGHT_DRIVE_ENCODER_CHANNEL_A = 2;
-    public static final int RIGHT_DRIVE_ENCODER_CHANNEL_B = 3;
-    public static final boolean RIGHT_DRIVE_ENCODER_IS_INVERTED = false;
-    public static final EncodingType DRIVE_MOTOR_ENCODER_ENCODINGTYPE = null;
+        public static final int LEFT_DRIVE_ENCODER_CHANNEL_A = 0;
+        public static final int LEFT_DRIVE_ENCODER_CHANNEL_B = 1;
+        public static final int RIGHT_DRIVE_ENCODER_CHANNEL_A = 2;
+        public static final int RIGHT_DRIVE_ENCODER_CHANNEL_B = 3;
+        public static final boolean RIGHT_DRIVE_ENCODER_IS_INVERTED = false;
+        public static final EncodingType DRIVE_MOTOR_ENCODER_ENCODINGTYPE = null;
 
-    //public static final EncodingType DRIVE_MOTOR_ENCODER_ENCODINGTYPE = EncodingType.k2X;
+        //public static final EncodingType DRIVE_MOTOR_ENCODER_ENCODINGTYPE = EncodingType.k2X;
 
-    //public static final boolean LEFT_DRIVE_ENCODER_IS_INVERTED = false;
-    //public static final boolean RIGHT_DRIVE_ENCODER_IS_INVERTED = true;
+        //public static final boolean LEFT_DRIVE_ENCODER_IS_INVERTED = false;
+        //public static final boolean RIGHT_DRIVE_ENCODER_IS_INVERTED = true;
 
-    //public static final boolean SHOW_DRIVETRAIN_IN_DASHBOARD = true;
+        //public static final boolean SHOW_DRIVETRAIN_IN_DASHBOARD = true;
 
-   // public static final double SET_MAX_RATE = 0.5;
-}
+        // public static final double SET_MAX_RATE = 0.5;
+    }
 }
