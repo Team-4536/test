7f58f20324b27822a25572f519e184d85bc5202a
squidward tentacles <pappasgavriel@gmail.com>
2022-12-05 20:32:00 -0600

    write the PID linkage to command

--- src/main/java/frc/robot/commands/LinkageTo.java
+++ src/main/java/frc/robot/commands/LinkageTo.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.LinkageInfo;
 import frc.robot.subsystems.LinkageSystem;
 
 public class LinkageTo extends CommandBase{
@@ -22,6 +24,51 @@ public class LinkageTo extends CommandBase{
         m_goalPosition = goalPos;
 
         m_pid = new PIDController(kP, kI, kD);
+
+    }
+
+    @Override
+    public void initialize() {
+        
+        m_pid.reset();
+
+    }
+
+    @Override
+    public void execute() {
+        
+        double currentPosition = m_linkage.getEncoderValue();
+        double error = currentPosition - m_goalPosition;
+
+        if (currentPosition < LinkageInfo.LINKAGE_MIDPOINT_POSITION){
+
+            m_linkage.runX(.7);
+
+        }
+        else {
+
+            double PIDSpeed = m_pid.calculate(error);
+            double LinkageSpeed = Math.min(PIDSpeed, .7);
+
+            m_linkage.runX(LinkageSpeed);
+
+            SmartDashboard.putNumber("PID val linkage", PIDSpeed);
+
+
+        }
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        
+        return m_linkage.getEncoderValue() >= m_goalPosition;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        
+        m_linkage.setGoalPos(m_linkage.getEncoderValue());
 
     }
     
--- src/main/java/frc/robot/subsystems/LinkageSystem.java
+++ src/main/java/frc/robot/subsystems/LinkageSystem.java
@@ -37,6 +37,7 @@ public class LinkageSystem extends SubsystemBase{
        m_linkageMotor.set(speed);
 
        SmartDashboard.putNumber("Lift Encoder", m_encoder.getPosition());
+       SmartDashboard.putNumber("Spark Motor Output", speed);
 
     }
 
