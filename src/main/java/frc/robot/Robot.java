e3ecf28a00cc6174dfdd9a5717edbfa51a57c027
squidward tentacles <pappasgavriel@gmail.com>
2022-12-07 08:50:24 -0600

    maybe do vision stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,12 @@
 
 package frc.robot;
 
+import org.opencv.core.Mat;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.AxisCamera;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -18,6 +24,7 @@ public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private RobotContainer m_robotContainer;
+    private Thread m_visionThread;
 
     /**
      * This function is run when the robot is first started up and should be used for any
@@ -28,6 +35,30 @@ public class Robot extends TimedRobot {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
+
+        /*
+        m_visionThread = new Thread(
+            () -> {
+                AxisCamera camera = CameraServer.addAxisCamera("10.45.36.15");
+                camera.setResolution(640, 480);
+
+                CvSink cvSink = CameraServer.getVideo();
+                CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
+
+                Mat mat = new Mat();
+
+                while (!Thread.interrupted()) {
+                    if (cvSink.grabFrame(mat) == 0) {
+                        outputStream.notifyError(cvSink.getError());
+                        continue;
+                    }
+                    outputStream.putFrame(mat);
+                }
+            });
+        m_visionThread.setDaemon(true);
+        m_visionThread.start();
+        */
+
     }
 
     /**
